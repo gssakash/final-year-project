23c5b3d83a478db83f8c853eab9d3087c5950ef2
gssakash <gsaisriram.akash@gmail.com>
2023-05-05 16:55:50 +0530

    fixed and added image upload capability

--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
 import './ImageUploader.css';
 
 function ImageUploader() {
   const [image, setImage] = useState(null);
+  const [file, setFile] = useState(null);
+  const fileInputRef = useRef(); // Add this line to create a reference to the file input
 
   function handleImageChange(event) {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
+      setFile(selectedFile);
       const reader = new FileReader();
       reader.readAsDataURL(selectedFile);
       reader.onload = () => {
@@ -16,25 +19,39 @@ function ImageUploader() {
     }
   }
 
-  //Reference : https://dev.to/shadid12/how-to-upload-images-to-s3-in-a-react-application-4lm
-  function handleImageSubmit(event) {
-    event.preventDefault();
-    console.log('Uploading image:', image);
-    
+  async function handleImageSubmit() {
+    if (file) {
+      const fileName = file.name;
+      console.log('File name: ', fileName);
+      let url = `http://127.0.0.1:8000/predict/{fileName}`
+      const response = await axios.get(url);
+      console.log('API Response: ', response.data)
+      console.log(response.data.prediction)
+    }
+  }
+
+  function triggerFileInput() {
+    fileInputRef.current.click(); // Trigger the hidden file input
   }
 
   return (
     <div>
       <h1 className="image-upload-text">Image Uploader</h1>
-      <form onSubmit={handleImageSubmit}>
-        <input type="file" accept=".jpg, .jpeg, .png" onChange={handleImageChange} />
-        {image && (
-          <div>
-            <h2>Preview:</h2>
-            <img src={image} alt="Uploaded image" width="200" />
-          </div>
-        )}
-      </form>
+      <input
+        type="file"
+        ref={fileInputRef}
+        accept=".jpg, .jpeg, .png"
+        onChange={handleImageChange}
+        style={{ display: 'none' }} // Hide the file input element
+      />
+      {image && (
+        <div>
+          <h2>Preview:</h2>
+          <img src={image} alt="Uploaded image" width="200" />
+        </div>
+      )}
+      <button id="selector-btn" onClick={triggerFileInput}>Select Image</button>
+      <button id="uploader-btn" onClick={handleImageSubmit}>Upload Image</button>
     </div>
   );
 }

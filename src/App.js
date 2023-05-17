673923c7679bb10fa7ddce549b67e64a02eaf367
Kabilan <kabilanvennila@gmail.com>
2023-05-17 11:42:38 +0530

    PDF gen

--- src/App.js
+++ src/App.js
@@ -9,14 +9,22 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h2>Early detection of ocular diseases is crucial in preventing blindness caused by conditions such as diabetes, glaucoma, cataract, age-related macular degeneration (AMD), and many other diseases. With at least 2.2 billion people around the world currently experiencing vision impairments, it is vital to develop cost-effective and efficient methods to detect these diseases early on. One such method is through the use of computer vision and deep learning, which can automatically detect ocular diseases by analyzing high-quality medical eye fundus images. By utilizing this technology, healthcare professionals can accurately and quickly diagnose ocular diseases, allowing for early intervention and treatment. This, in turn, can help reduce the incidence of blindness and improve the quality of life for those affected by ocular diseases.
-</h2>
+      <p>Early detection of ocular diseases is crucial in preventing blindness caused by conditions such as diabetes, glaucoma, cataract, age-related macular degeneration (AMD), and many other diseases. With at least 2.2 billion people around the world currently experiencing vision impairments, it is vital to develop cost-effective and efficient methods to detect these diseases early on. One such method is through the use of computer vision and deep learning, which can automatically detect ocular diseases by analyzing high-quality medical eye fundus images. By utilizing this technology, healthcare professionals can accurately and quickly diagnose ocular diseases, allowing for early intervention and treatment. This, in turn, can help reduce the incidence of blindness and improve the quality of life for those affected by ocular diseases.
+</p>
       <img src='https://www.samyakeyecare.com/wp-content/uploads/2021/12/fundus-1024x614.jpg' alt="main-funds"></img>
       <br/>
       <img src={firstOutput} alt="myimage-1`"></img>
       <br/>
       <br/>
-      <p>Lorem ipsum dolor</p>
+      <p>The age distribution of eye diseases can vary depending on the specific condition. Some eye diseases are more common in older age groups, while others can affect individuals at any age.
+
+For example, age-related macular degeneration (AMD) is a leading cause of vision loss in people over the age of 50. As the name suggests, this condition is strongly associated with aging, and the risk of developing AMD increases significantly after the age of 60. However, there are also cases of AMD occurring in individuals in their 40s or even younger.
+
+Cataracts are another common eye condition that is more prevalent in older age groups. According to the National Eye Institute, by age 80, more than half of all Americans either have a cataract or have had cataract surgery. However, cataracts can also occur in younger individuals due to factors such as genetics or trauma.
+
+On the other hand, conditions like amblyopia (lazy eye) and strabismus (crossed eyes) are more commonly diagnosed in childhood. These conditions can lead to vision problems if left untreated, but early detection and intervention can often improve outcomes.
+
+Overall, the age distribution of eye diseases can vary widely depending on the specific condition. Regular eye exams and early detection can be important for maintaining good vision and preventing vision loss, regardless of age.</p>
       <img src={setofeyes} alt="myimage-1`"></img>
       <ImageUploader />
       <h3>Project Done by Akash GSS and Kabilan M under the guidance of Dr.Rajaram V</h3>
--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,13 +1,43 @@
 import React, { useRef, useState } from 'react';
 import axios from 'axios';
+import { StyleSheet, PDFDownloadLink, Image, Document, Page, Text } from '@react-pdf/renderer';
+ 
 import './ImageUploader.css';
+ 
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: '#FFFFFF',
+    padding: 40,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  image: {
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 12,
+    marginBottom: 10,
+    color: "purple"
+  },
+  date: {
+    paddingLeft: "80%"
+  }
+});
+
+  let date = new Date().toLocaleDateString();
 
 function ImageUploader() {
   const [image, setImage] = useState(null);
   const [file, setFile] = useState(null);
   const [pred,setPred] = useState(null);
   const fileInputRef = useRef(); // Add this line to create a reference to the file input
-
+ 
   function handleImageChange(event) {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
@@ -16,10 +46,34 @@ function ImageUploader() {
       reader.readAsDataURL(selectedFile);
       reader.onload = () => {
         setImage(reader.result);
+ 
       };
     }
   }
+  
 
+  function MyDocument(props) {
+  return (
+    <Document>
+      <Page style={styles.page}>
+        <Text style={styles.date}>{date}</Text>
+        <Text style={styles.title}> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -</Text>
+        <Text style={styles.title}> ODIR - Eye Report</Text>
+        <Text style={styles.gen}> Name : Jhon doe</Text>
+        <Text style={styles.gen}> Date of Birth : 9/07/1995</Text>
+        <Text style={styles.gen}> Blood Group : O -ve </Text>
+        <Text style={styles.gen}> Consulted Doctor : Walter Green </Text>
+        <Text style={styles.gen}> Address - 7th Avenue, New York Castle, New York, US - 60002904</Text>
+        <Text style={styles.title}> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -</Text>
+        <Image style={styles.image} src={props.image} />
+        <Text style={styles.title}> - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -</Text>
+        <Text style={styles.subtitle}>API Response:</Text>
+        <Text style={styles.text}>{props.data}</Text>
+      </Page>
+    </Document>
+  );
+}
+ 
   async function handleImageSubmit() {
     if (file) {
       const fileName = file.name;
@@ -35,11 +89,11 @@ function ImageUploader() {
       setPred(response.data.prediction)
     }
   }
-
+ 
   function triggerFileInput() {
     fileInputRef.current.click(); // Trigger the hidden file input
   }
-
+ 
   return (
     <div>
       <h1 className="image-upload-text">Image Uploader</h1>
@@ -58,9 +112,15 @@ function ImageUploader() {
       )}
       <button id="selector-btn" onClick={triggerFileInput}>Select Image</button>
       <button id="uploader-btn" onClick={handleImageSubmit}>Upload Image</button>
+      <PDFDownloadLink document={<MyDocument data={pred} image={image} />} fileName="report.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? "Loading document..." : "Download Pdf"
+        }
+      </PDFDownloadLink>
+ 
       <h2>{pred}</h2>
     </div>
   );
 }
-
+ 
 export default ImageUploader;
